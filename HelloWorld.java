72db00b57907a1d5c815a6b9c06d3155333d4eba
Yaolong Zhu <yaolong.zhu@sportsbet.com.au>
2023-12-06 21:59:52 +1100

    add new-feature-2 commit 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public Class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("new-feature-1");
+		System.out.prontln("new-feature-2");
 	}
 }
