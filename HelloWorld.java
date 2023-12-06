a36f87c6d867a3fb5e6bacb1940141ab1f3eac25
Yaolong Zhu <144290098+yaolongz-sportsbet@users.noreply.github.com>
2023-12-06 22:07:07 +1100

    Merge new-feature-2 to dev with "squash and merge"
    * add new-feature-2 commit 1
    
    * add new-feature-2 commit 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public Class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("new-feature-1");
+		System.out.prontln("new-feature-2");
+		System.out.prontln("new-feature-2");
 	}
 }
